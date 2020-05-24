774c280758d30f7a279431c78aaf82c19d6ec0a0
thanos <thanos.karousos@gmail.com>
2020-05-24 17:13:33 +0300

    initialized classes.js

---
+++ project/classes.js
@@ -0,0 +1,19 @@
+class team {
+    constructor(name) {
+      this.teamName = name;
+    }
+
+    addMember(member) {
+        //adding member code
+        
+        return ;
+      }  
+
+    removeMember(member){
+        //removing member code
+        
+        return ;
+    }
+
+}
+  

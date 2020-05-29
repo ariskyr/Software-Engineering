e9a33e73fce436bf067995683f12594897bd0210
Likecinema <mauksalah@yahoo.gr>
2020-05-29 22:13:46 +0300

    added Message and Logs template

--- project/classes.js
+++ project/classes.js
@@ -1,5 +1,6 @@
 // use "#"" for private
 // use "static" for public
+//Added exports for classes
 
 class Team {
   constructor(name) {
@@ -19,24 +20,27 @@ class Team {
   }
 
 }
+module.exports = Team;
 
-class TeamList{
-  
+
+class TeamList {
+
   static teamMembers(Team) {
     team.forEach(Member => { //den einai se array omws
       return Member;
     });
   }
 
-  static makeAdmin(Member){
+  static makeAdmin(Member) {
     member.isAdmin = true;
   }
 }
 
+
 //team.TeamList.push(teamObject); because of domain model
 
-class Member{
-  constructor(id, name, username, password){
+class Member {
+  constructor(id, name, username, password) {
     this.id = id;
     this.name = name;
     this.username = username;
@@ -46,17 +50,17 @@ class Member{
 
   static status;
 
-  isAdminOnline(){
+  isAdminOnline() {
 
-    if(this.isAdmin){
+    if (this.isAdmin) {
       return status;
-    
-    }else{
+
+    } else {
       //error code
     }
   }
 
-  isOnline(){
+  isOnline() {
     return status;
   }
 
@@ -120,6 +124,54 @@ class projectStatus {
   }
 }
 
+class File {
+  //do not forget to add path to attributes
+  constructor(extension, path) {
+    this.extension = extension;
+    this.path = path;
+  }
+  rename(name) {
+    try {
+      //TODO do nodejs stuff to actually rename the file
+      name = new File(this.extension, this.path)
+      //this = null;
+      return true;
+    } catch (e) {
+      return Error("Could not rename the file")
+    }
+  }
+
+  uploadFile() {
+    if (fileTooLarge) {
+      //error
+
+    } else {
+      //upload to videocall
+    }
+
+  }
+}
+
+class Folder extends File {
+  //constructor takes 0 arguments. lastEdited is today. No files,obviously
+  constructor(filelist) {
+    this.lastEdited = new Date();
+    this.fileList = new Array();
+    if (filelist === undefined) {} else this.fileList = fileList
+
+  }
+  //do not forget to remove createFolder from domain-model. It's useless
+  renameFolder(name) {
+    name = new Folder(this.fileList);
+    //this = null;
+    //TODO do nodejs stuff to actually rename the folder.
+  }
+  deleteFolder() { //deleteFolder shouldn't take arguments
+    //TODO do nodejs stuff to actually delete the folder
+    //this = null;
+  }
+}
+
 class gitRepository extends Folder {
   constructor(name, path, description, owner, dateCreated, commits) {
     this.name = name;
@@ -147,77 +199,46 @@ class gitRepository extends Folder {
   }
 }
 
-class Folder extends File{
-  //constructor takes 0 arguments. lastEdited is today. No files,obviously
-  constructor(){
-    this.lastEdited = new Date();
-    this.fileList = new Array();
-  }
-  //constructor on rename takes arguments to dublicate the file
-  constructor(fileList){
-    this.lastEdited = new Date;
-    this.fileList = fileList;
-  }
-  //do not forget to remove createFolder from domain-model. It's useless
-  renameFolder(name){
-    name = new Folder(this.fileList);
-    this = null;
-    //TODO do nodejs stuff to actually rename the folder.
-  }
-  deleteFolder(){ //deleteFolder shouldn't take arguments
-    //TODO do nodejs stuff to actually delete the folder
-    this = null;
-  }
-}
-
-class File{
-  //do not forget to add path to attributes
-  constructor(extension, path){
-    this.extension = extension;
-    this.path = path;
-  }
-  rename(name){
-    try {
-    //TODO do nodejs stuff to actually rename the file
-    name = new File(this.extension, this.path)
-    this = null;
-    return true;
-    }
-    catch(e){
-      return Error("Could not rename the file")
-    }
-  }
-
-  uploadFile(){
-    if(fileTooLarge){
-      //error
-
-    }else{
-      //upload to videocall
-    }
-
-  }
-}
 
 
-class CallWindow{ //extends BrowserWindow
-  constructor(callId, duration, startedAt, callStartedBy){
+
+class CallWindow { //extends BrowserWindow
+  constructor(callId, duration, startedAt, callStartedBy) {
     this.callId = callId;
     this.duration = duration;
     this.startedAt = startedAt;
     this.callStartedBy = callStartedBy;
   }
 
-  muteSelf(member){
+  muteSelf(member) {
     member.voiceVolume = 0; //logika kapws etsi tha ginetai
   }
 
-  unmuteSelf(member){
+  unmuteSelf(member) {
     //fetch previous voiceVolume
     //member.voiceVolume = previous voiceVolume
   }
 }
 
-class VideoCallInfo{ //isws na mhn prepei na einai klasi,
+class VideoCallInfo { //isws na mhn prepei na einai klasi,
   //den to exoume sto domain, einai sto robustness UC4
+}
+
+class Message {
+  constuctor(text, date, sender) {
+    this.text = text;
+    this.sender = sender
+    this.dateSent = date
+  }
+  //TODO add functions
+}
+
+class Logs {
+  constructor(info, timestamp, comments) {
+    this.info = info //this should be of type "message", "timestamp" is useless, do not forget
+    this.comments = comments;
+  }
+  clear() {
+    //this = null;
+  }
 }

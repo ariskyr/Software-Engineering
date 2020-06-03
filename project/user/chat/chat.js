654b602ee1488c14624939a6ff375663c2b96ef6
thanos <thanos.karousos@gmail.com>
2020-06-04 00:13:26 +0300

    added some chat ui

---
+++ project/user/chat/chat.js
@@ -0,0 +1,7 @@
+function sendText(){
+    
+    document.getElementById("messageLog").innerHTML = document.getElementById("messageBox").value;
+    document.getElementById("messageBox").value = "This is cleared"; 
+
+    // pros to paron emfanizetai mono ena minima, ara den einai akrivws logs
+}

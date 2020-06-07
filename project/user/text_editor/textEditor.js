fa31f209f39a5d6f7d5e5f3c01fd102ac80dd5b9
thanos <thanos.karousos@gmail.com>
2020-06-07 14:44:54 +0300

    text editorUI

---
+++ project/user/text_editor/textEditor.js
@@ -0,0 +1,15 @@
+function makeBold() {
+    document.getElementById('content').classList.toggle('bold'); 
+}
+
+function makeItalics(){
+    document.getElementById('content').classList.toggle('italics');
+}
+
+function makeUnderline(){
+    document.getElementById('content').classList.toggle('underline');
+}
+
+function makeLineThrough(){
+    document.getElementById('content').classList.toggle('lineThrough');
+}
